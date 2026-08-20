46b760a5e3ecc74530059ddcf89f3379325d904c
rahmotpur-news-bd <hasanuzzamanmamun773@gmail.com>
2026-08-20 15:14:58 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
 const CACHE_NAME = "rahmotpur-news-v1";
 
-const APP_FILES = [
+const OFFLINE_FILES = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon.svg"
 ];
+
 
 /* =========================
    INSTALL
@@ -17,7 +18,7 @@ self.addEventListener("install", function(event) {
 
     caches.open(CACHE_NAME).then(function(cache) {
 
-      return cache.addAll(APP_FILES);
+      return cache.addAll(OFFLINE_FILES);
 
     })
 
@@ -36,15 +37,15 @@ self.addEventListener("activate", function(event) {
 
   event.waitUntil(
 
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function(names) {
 
       return Promise.all(
 
-        cacheNames.map(function(cacheName) {
+        names.map(function(name) {
 
-          if (cacheName !== CACHE_NAME) {
+          if(name !== CACHE_NAME) {
 
-            return caches.delete(cacheName);
+            return caches.delete(name);
 
           }
 
@@ -67,47 +68,182 @@ self.addEventListener("activate", function(event) {
 
 self.addEventListener("fetch", function(event) {
 
-  if (event.request.method !== "GET") {
+  if(event.request.method !== "GET") {
+
     return;
+
   }
 
   event.respondWith(
 
     fetch(event.request)
+
       .then(function(response) {
 
-        if (
-          response &&
-          response.status === 200 &&
-          response.type !== "opaque"
-        ) {
+        if(response && response.status === 200) {
 
-          const responseClone =
-            response.clone();
+          const copy = response.clone();
 
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
+          caches.open(CACHE_NAME).then(function(cache) {
 
-              cache.put(
-                event.request,
-                responseClone
-              );
+            cache.put(event.request, copy);
 
-            });
+          });
 
         }
 
         return response;
 
       })
+
       .catch(function() {
 
-        return caches.match(
-          event.request
-        );
+        return caches.match(event.request);
 
       })
 
   );
 
 });
+
+
+/* =========================
+   FIREBASE NOTIFICATION
+========================= */
+
+self.addEventListener(
+  "push",
+  function(event) {
+
+    let data = {};
+
+    try {
+
+      if(event.data) {
+
+        data = event.data.json();
+
+      }
+
+    } catch(error) {
+
+      data = {
+
+        notification: {
+
+          title: "Rahmotpur News",
+
+          body: event.data
+            ? event.data.text()
+            : "নতুন খবর প্রকাশিত হয়েছে।"
+
+        }
+
+      };
+
+    }
+
+
+    const notification =
+      data.notification || data;
+
+
+    const title =
+      notification.title ||
+      "Rahmotpur News";
+
+
+    const options = {
+
+      body:
+        notification.body ||
+        "নতুন খবর প্রকাশিত হয়েছে।",
+
+      icon:
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjubv8pdof9w5KdiSeB5TV8e93OYhQ8nhBcO3RRgSW8GyPr9ysInzD3GCU_B7K74qHbnZHJdcLuaqVRNACbfhk4DLCpg6nbweylNnfSgRBAJXoMdyXuYLZ-aLEWp3Yt4qwh-U1gx5UoXs49xMKeA6w7BLVH1lvjGzSCHH3ayVQKSbdOpQ/s1600/FB_IMG_1786278216091.jpg",
+
+      badge:
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjubv8pdof9w5KdiSeB5TV8e93OYhQ8nhBcO3RRgSW8GyPr9ysInzD3GCU_B7K74qHbnZHJdcLuaqVRNACbfhk4DLCpg6nbweylNnfSgRBAJXoMdyXuYLZ-aLEWp3Yt4qwh-U1gx5UoXs49xMKeA6w7BLVH1lvjGzSCHH3ayVQKSbdOpQ/s1600/FB_IMG_1786278216091.jpg",
+
+      data: {
+
+        url:
+          notification.click_action ||
+          "https://rahmotpur-news-bd.github.io/rahmotpur-news-pwa/"
+
+      },
+
+      requireInteraction: false,
+
+      vibrate: [200, 100, 200]
+
+    };
+
+
+    event.waitUntil(
+
+      self.registration.showNotification(
+        title,
+        options
+      )
+
+    );
+
+  }
+);
+
+
+/* =========================
+   NOTIFICATION CLICK
+========================= */
+
+self.addEventListener(
+  "notificationclick",
+  function(event) {
+
+    event.notification.close();
+
+
+    const url =
+      event.notification.data &&
+      event.notification.data.url
+        ? event.notification.data.url
+        : "https://rahmotpur-news-bd.github.io/rahmotpur-news-pwa/";
+
+
+    event.waitUntil(
+
+      clients.matchAll({
+
+        type: "window",
+        includeUncontrolled: true
+
+      }).then(function(clientList) {
+
+        for(
+          const client of clientList
+        ) {
+
+          if(
+            client.url.includes(
+              "rahmotpur-news-bd.github.io"
+            )
+          ) {
+
+            client.focus();
+
+            return client.navigate(url);
+
+          }
+
+        }
+
+
+        return clients.openWindow(url);
+
+      })
+
+    );
+
+  }
+);
